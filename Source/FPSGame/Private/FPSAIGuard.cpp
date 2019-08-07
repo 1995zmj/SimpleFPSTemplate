5bb2f66d9d0c749df1c21b81a7942092c955eb13
1995zmj <minjiangzhao@icloud.com>
2019-08-07 19:59:17 +0800

    4-P8

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -4,6 +4,7 @@
 #include "FPSAIGuard.h"
 #include "Perception/PawnSensingComponent.h"
 #include "DrawDebugHelpers.h"
+#include "FPSGameMode.h"
 
 // Sets default values
 AFPSAIGuard::AFPSAIGuard()
@@ -32,6 +33,12 @@ void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 		return;
 
 	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Yellow, false, 10.0f);
+
+	AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+	if (GM)
+	{
+		GM->CompleteMission(SeenPawn, true);
+	}
 }
 
 void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume)
